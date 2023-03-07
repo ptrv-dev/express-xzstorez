d4bc8f99f43bf7001a6aef020bf9cf4b32523287
ptrv-dev <petrov99119@gmail.com>
2023-03-07 16:47:45 +0200

    Product Controller - Get all function

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -4,6 +4,7 @@ import { validationResult } from 'express-validator';
 import ProductModel from '../models/ProductModel';
 
 import { productCreateBody } from '../@types/requestBody';
+import { productGetAllQuery } from '../@types/requestQuery';
 
 export async function create(
   req: Request<{}, {}, productCreateBody>,
@@ -29,6 +30,29 @@ export async function create(
     return res.status(200).json(product);
   } catch (error) {
     console.log(`[Error] Product create error!\n${error}\n\n`);
+    return res.sendStatus(500);
+  }
+}
+
+export async function getAll(
+  req: Request<{}, {}, {}, productGetAllQuery>,
+  res: Response
+) {
+  try {
+    const { q = '', category, brand, sort, order } = req.query;
+
+    const products = await ProductModel.find({
+      title: { $regex: new RegExp(q), $options: 'i' },
+      category: category || undefined,
+      brand: brand || undefined,
+    })
+      .sort({ [sort || 'createdAt']: order || 'desc' })
+      .limit(20)
+      .populate('brand category');
+
+    return res.status(200).json(products);
+  } catch (error) {
+    console.log(`[Error] Product get all error!\n${error}\n\n`);
     return res.sendStatus(500);
   }
 }
