16e4ec3ab0832689a9814c81b26defeb4ccf5875
ptrv-dev <petrov99119@gmail.com>
2023-03-07 17:22:47 +0200

    Category Edit Validation

--- src/validations/category.validation.ts
+++ src/validations/category.validation.ts
@@ -3,3 +3,7 @@ import { body } from 'express-validator';
 export const categoryCreateValidation = [
   body('title').isLength({ min: 4, max: 128 }),
 ];
+
+export const categoryEditValidation = [
+  body('title').optional().isLength({ min: 4, max: 128 }),
+];
