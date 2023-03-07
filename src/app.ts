002d8c8384c4aec5c5ced23a38cafdd9763512db
ptrv-dev <petrov99119@gmail.com>
2023-03-07 10:13:06 +0200

    Multer confied + PORT constant set

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,22 @@
 import express from 'express';
+import multer from 'multer';
 import dotenv from 'dotenv';
 
 // Dotenv config
 dotenv.config();
+
+// constants
+const PORT = process.env.PORT || 5004;
+
+// multer config
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}--${file.originalname}`);
+  },
+});
 
 const app = express();
 
