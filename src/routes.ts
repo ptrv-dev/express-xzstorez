06c992e623c7c94684335041f2c0a6c7852e95a4
ptrv-dev <petrov99119@gmail.com>
2023-03-07 15:02:25 +0200

    Routes - Upload one

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express';
+import { upload } from './app';
 
 // controllers
 import * as AuthController from './controllers/auth.controller';
 import * as CategoryController from './controllers/category.controller';
+import * as UploadController from './controllers/upload.controller';
 
 // middleware
 import { verifyToken } from './middlewares/verifyToken';
@@ -12,6 +14,13 @@ import { loginValidation } from './validations/auth.validation';
 import { categoryCreateValidation } from './validations/category.validation';
 
 export default function (app: Express) {
+  // upload routes
+  app.post(
+    '/upload',
+    verifyToken,
+    upload.single('image'),
+    UploadController.uploadOne
+  );
   // auth routes
   app.post('/auth/login', loginValidation, AuthController.login);
   // category routes
