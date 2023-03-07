79c890aaa1e52b996b3c8c1422f4d842edc64097
ptrv-dev <petrov99119@gmail.com>
2023-03-07 14:17:34 +0200

    Routes - Category create

--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,23 @@ import { Express } from 'express';
 
 // controllers
 import * as AuthController from './controllers/auth.controller';
+import * as CategoryController from './controllers/category.controller';
 
 // middleware
 import { verifyToken } from './middlewares/verifyToken';
 
 // validations
 import { loginValidation } from './validations/auth.validation';
+import { categoryCreateValidation } from './validations/category.validation';
 
 export default function (app: Express) {
+  // auth routes
   app.post('/auth/login', loginValidation, AuthController.login);
+  // category routes
+  app.post(
+    '/category',
+    verifyToken,
+    categoryCreateValidation,
+    CategoryController.create
+  );
 }
