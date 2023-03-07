0fdf5cf40748517d5afe2ed32aa8253c5d131113
ptrv-dev <petrov99119@gmail.com>
2023-03-07 16:05:55 +0200

    Product Create validation

---
+++ src/validations/product.validation.ts
@@ -0,0 +1,11 @@
+import { body } from 'express-validator';
+
+export const productCreateValidation = [
+  body('images').isArray(),
+  body('title').isLength({ min: 3, max: 256 }),
+  body('description').optional().isLength({ min: 3, max: 4096 }),
+  body('category').optional().isString(),
+  body('brand').optional().isString(),
+  body('sizes').optional().isArray(),
+  body('price').isFloat({ min: 0, max: 999999 }),
+];
