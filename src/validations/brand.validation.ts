1d78b91b31d279642685a736b07dab580e791fa1
ptrv-dev <petrov99119@gmail.com>
2023-03-07 17:24:01 +0200

    Brand Edit Validation

--- src/validations/brand.validation.ts
+++ src/validations/brand.validation.ts
@@ -4,3 +4,7 @@ export const brandCreateValidation = [
   body('image').isString(),
   body('title').isLength({ min: 3, max: 128 }),
 ];
+export const brandEditValidation = [
+  body('image').optional().isString(),
+  body('title').optional().isLength({ min: 3, max: 128 }),
+];
